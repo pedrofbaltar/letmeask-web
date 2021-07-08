304cdff58ad8816c2785369024177be9ed66d944
pedrofbaltar <pfbaltariead@gmail.com>
2021-07-08 09:19:17 -0300

    Routes navigation and firebase auth

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,28 @@
+import { useHistory } from "react-router-dom";
+
+import { auth, firebase } from "../services/firebase";
+
 import IllustrationImg from "../assets/images/illustration.svg";
 import LogoImg from "../assets/images/logo.svg";
 import GoogleIconImg from "../assets/images/google-icon.svg";
 
-import "../styles/auth.scss";
 import { Button } from "../components/Button";
 
+import "../styles/auth.scss";
+
 export function Home() {
+  const history = useHistory();
+
+  function handleCreateRoom() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(provider).then((result) => {
+      console.log(result);
+
+      history.push("/rooms/new");
+    });
+  }
+
   return (
     <div id="page-auth">
       <aside>
@@ -21,7 +38,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={LogoImg} alt="Letmeask" />
-          <button className="create-room">
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={GoogleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 import IllustrationImg from "../assets/images/illustration.svg";
 import LogoImg from "../assets/images/logo.svg";
-import GoogleIconImg from "../assets/images/google-icon.svg";
+
+import { Button } from "../components/Button";
 
 import "../styles/auth.scss";
-import { Button } from "../components/Button";
 
 export function NewRoom() {
   return (
@@ -27,7 +29,7 @@ export function NewRoom() {
             <Button type="submit">Criar sala</Button>
           </form>
           <p>
-            Quer entrar em uma sala existente? <a href="#">Clique aqui.</a>
+            Quer entrar em uma sala existente? <Link to="/">Clique aqui.</Link>
           </p>
         </div>
       </main>
